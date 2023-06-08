6001fd199f32c8913a01b343c710772d76fe60d7
Paweł gabryel <pawel.gabryel@apps.clinic>
2023-06-08 11:22:42 +0200

    tests

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  transformIgnorePatterns: ['/node_modules/(?!axios)'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-};
